92b8de1407b26d89d0ac9aa09f71dc0838830e8e
Ganom <8338284+Ganom@users.noreply.github.com>
2025-07-23 22:58:47 -0400

    refactor(app): implement view locator, cache views, and simplify DI

--- SimpleTwitchEmoteSounds/ViewLocator.cs
+++ SimpleTwitchEmoteSounds/ViewLocator.cs
@@ -1,32 +1,43 @@
 using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SimpleTwitchEmoteSounds.Common;
 using SimpleTwitchEmoteSounds.ViewModels;
 
 namespace SimpleTwitchEmoteSounds;
 
-public class ViewLocator : IDataTemplate
+public class ViewLocator(AppViews views) : IDataTemplate
 {
-    public Control? Build(object? data)
+    private readonly Dictionary<object, Control> _controlCache = [];
+
+    public Control Build(object? param)
     {
-        if (data is null)
-            return null;
-
-        var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
-
-        if (type != null)
+        if (param is null)
         {
-            var control = (Control)Activator.CreateInstance(type)!;
-            control.DataContext = data;
+            return CreateText("Data is null.");
+        }
+
+        if (_controlCache.TryGetValue(param, out var control))
+        {
             return control;
         }
 
-        return new TextBlock { Text = "Not Found: " + name };
+        if (views.TryCreateView(param, out var view))
+        {
+            _controlCache.Add(param, view);
+
+            return view;
+        }
+
+        return CreateText($"No View For {param.GetType().Name}.");
     }
 
-    public bool Match(object? data)
+    public bool Match(object? data) => data is ObservableObject;
+
+    private static TextBlock CreateText(string text) => new TextBlock
     {
-        return data is ViewModelBase;
-    }
+        Text = text
+    };
 }
